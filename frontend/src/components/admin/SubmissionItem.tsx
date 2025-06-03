bd36a6e34e6f34a71d39f2fd1eb93c935144b9ef
Archit jain <151944243+Archit-0@users.noreply.github.com>
2025-06-03 21:52:03 +0530

    Update SubmissionItem.tsx

--- frontend/src/components/admin/SubmissionItem.tsx
+++ frontend/src/components/admin/SubmissionItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { Submission } from "../../interfaces/Submission";
 import { approveSubmission, rejectSubmission } from "../../api/adminApi";
 
